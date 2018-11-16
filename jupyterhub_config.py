90cc93df0184bbf26bfd9f0fd182632aac591e6f
Ubuntu <ubuntu@ip-172-31-16-210.ec2.internal>
2018-11-16 21:45:53 +0000

    .

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -141,7 +141,7 @@ c.JupyterHub.port = 80
 c.Auth0OAuthenticator.client_id = os.environ['AUTH0_CLIENT_ID']
 c.Auth0OAuthenticator.client_secret = os.environ['AUTH0_CLIENT_SECRET']
 #c.Auth0OAuthenticator.oauth_callback_url = 'https://jupyter.4dnucleome.org/hub/oauth_callback'
-c.Auth0OAuthenticator.oauth_callback_url = 'http://ec2-18-234-72-189.compute-1.amazonaws.com/hub/oauth_callback'
+c.Auth0OAuthenticator.oauth_callback_url = 'http://ec2-54-172-68-130.compute-1.amazonaws.com/hub/oauth_callback'
 c.JupyterHub.authenticator_class = 'oauthenticator.auth0.Auth0OAuthenticator'
 
 # Development authenticator
