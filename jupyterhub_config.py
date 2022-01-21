37514f9c0cac79fe3def8277d3b09ef027a72d42
William Ronchetti <wrr33@cornell.edu>
2022-01-21 11:13:15 -0500

    reorganize, refactor

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -9,25 +9,30 @@ from dcicutils import ff_utils, s3_utils
 from botocore.exceptions import ClientError
 
 
+# Initialize JH
 c = get_config()
-s3_client = boto3.client('s3')
 
 # get access keys and jupyterhub token for dcicutils. always use 'data' env
+s3_client = boto3.client('s3')
 s3_helper = s3_utils.s3Utils(env='data')
 ff_keys = s3_helper.get_ff_key()
 jh_token = s3_helper.get_jupyterhub_key()
+
+
+# Helper Functions
+
 
 def escape_string(in_str):
     """
     Escape a string the way DockerSpawner does, which is needed to make
     the user email match
-    NOTE: since dockerspawner 12.0, the old escape method is deprecated 
-    and in fact does not function at all since the referenced fields 
+    NOTE: since dockerspawner 12.0, the old escape method is deprecated
+    and in fact does not function at all since the referenced fields
     were removed.
     This change has to do with DNS/container naming and should be scrutinized.
     Dockerspawner claims a new convention is used, but can disable it.
     """
-    _docker_safe_chars = set(string.ascii_letters + string.digits + "-")
+    _docker_safe_chars = set(string.ascii_letters + string.digits + '-')
     _docker_escape_char = '_'
     return dockerspawner.dockerspawner.escape(
         in_str,
@@ -68,6 +73,8 @@ def initialize_user_content(spawner):
     access keys used for Jupyterhub for the user
     Also initialized a TrackingItem of type jupyterhub_session to track some
     basic information on the JH session
+    This function should also be heavily scrutinized, as it has been a source of
+    error in the past since we first acquire admin keys then context switch
     """
     err_output = []  # keep track of errors for debugging
 
@@ -84,10 +91,10 @@ def initialize_user_content(spawner):
         template_key = template_res['Key']
         user_subdir = 'user-' + escape_string(username)
         notebook_temp_key = '/'.join([user_subdir, template_key])
-        source_info = {"Bucket": os.environ['AWS_TEMPLATE_BUCKET'],
-                       "Key": template_key}
+        source_info = {'Bucket': os.environ['AWS_TEMPLATE_BUCKET'],
+                       'Key': template_key}
         try:  # always replace templates
-            s3_client.copy_object(Bucket=os.environ["AWS_NOTEBOOK_BUCKET"],
+            s3_client.copy_object(Bucket=os.environ['AWS_NOTEBOOK_BUCKET'],
                                   Key=notebook_temp_key, CopySource=source_info)
         except Exception as copy_exc:
             err_output.append({'copying_templates': str(copy_exc)})
@@ -172,79 +179,10 @@ def finalize_user_content(spawner):
 
 
 # JH Config options
-c.JupyterHub.log_level  = "DEBUG"
-# attach the hook functions to the spawner
-c.Spawner.pre_spawn_hook = initialize_user_content
-c.Spawner.post_stop_hook = finalize_user_content
-# propogate these variables to the user notebook processes
-c.Spawner.env_keep.extend(['FF_ACCESS_KEY', 'FF_ACCESS_SECRET',
-                           'INIT_ERR_OUTPUT', 'FF_TRACKING_ID'])
-# limit the memory use for single-user servers
-c.Spawner.mem_limit = '2G'
-# Spawn single-user servers as Docker containers
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-# Spawn containers from this image
-# by default, c.DockerSpawner.container_image uses jupyterhub/singleuser image
-# with the appropriate tag that pins version. Otherwise, do something like:
-# c.DockerSpawner.image = 'jupyter/scipy-notebook:8f56e3c47fec'
-c.DockerSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
-# default `start_singleuser.sh` is included
-c.DockerSpawner.cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
-# can I remove these two lines?
-# spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
-# c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
 
-# Configure idle culler service
-# This used to be a Python script we had to grab directly, but is now
-# fully integrated as a (non admin) hub service
-c.JupyterHub.services = [
-    {
-        'name': 'idle-culler',
-        'command': [sys.executable, '-m', 'jupyterhub_idle_culler', '--timeout=3600'],
-    }
-]
-c.JupyterHub.load_roles = [
-    {
-        "name": "list-and-cull", # name the role
-        "services": [
-            "idle-culler", # assign the service to this role
-        ],
-        "scopes": [
-            # declare what permissions the service should have
-            "list:users", # list users
-            "read:users:activity", # read user last-activity
-            "admin:servers", # start/stop servers
-        ],
-    }
-]
 
-# Connect containers to this Docker network
-network_name = 'bridge'  # unused?
-c.DockerSpawner.use_internal_ip = True
-c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME'] 
-c.DockerSpawner.escape = 'legacy'  # disable new escape behavior
-
-notebook_dir = os.environ['DOCKER_NOTEBOOK_DIR']
-c.DockerSpawner.notebook_dir = notebook_dir
-
-# https://stackoverflow.com/questions/51330356/jupyterhub-in-docker-container-not-able-to-connect-to-external-directory
-# notebook_mount_dir in form: '/path/on/host'
-notebook_mount_dir = '/home/ubuntu/data/jupyterhub-fourfront-notebooks/user-{username}'
-raw_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_RAW_FILE_BUCKET']
-open_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_OPEN_DATA_BUCKET']
-proc_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_PROC_FILE_BUCKET']
-# notebook_dir in form: '/path/on/container'
-c.DockerSpawner.volumes = {notebook_mount_dir: {"bind": notebook_dir, "mode": "rw"},
-                           raw_data_mount_dir: {"bind": '/home/jovyan/raw_data', "mode": "ro"},
-                           open_data_mount_dir: {"bind": '/home/jovyan/open_data', "mode": "ro"},
-                           proc_data_mount_dir: {"bind": '/home/jovyan/proc_data', "mode": "ro"}}
-
-# allow escaped characters in volume names
-c.DockerSpawner.format_volume_name = dockerspawner.volumenamingstrategy.escaped_format_volume_name
-# Remove containers once they are stopped
-c.DockerSpawner.remove = True
-# For debugging arguments passed to spawned containers
-c.DockerSpawner.debug = True
+# Logging
+c.JupyterHub.log_level  = 'DEBUG'
 
 # User containers will access hub by container name on the Docker network
 c.JupyterHub.hub_ip = 'jupyterhub'
@@ -253,38 +191,127 @@ c.JupyterHub.hub_port = 8080
 # TLS config
 c.JupyterHub.port = 80
 
+# Configure idle culler service (stop idle user containers)
+# This used to be a Python script we had to grab directly, but is now
+# fully integrated as a (non-admin) hub service
+c.JupyterHub.services = [
+    {
+        'name': 'idle-culler',
+        'command': [sys.executable, '-m', 'jupyterhub_idle_culler', '--timeout=3600'],
+    }
+]
+c.JupyterHub.load_roles = [
+    {
+        'name': 'list-and-cull', # name the role
+        'services': [
+            'idle-culler', # assign the service to this role
+        ],
+        'scopes': [
+            # declare what permissions the service should have
+            'list:users', # list users
+            'read:users:activity', # read user last-activity
+            'admin:servers', # start/stop servers
+        ],
+    }
+]
+
+# JH Authentication Config
+c.JupyterHub.authenticator_class = 'oauthenticator.auth0.Auth0OAuthenticator'
+
 # Production authenticator
 c.Auth0OAuthenticator.client_id = os.environ['AUTH0_CLIENT_ID']
 c.Auth0OAuthenticator.client_secret = os.environ['AUTH0_CLIENT_SECRET']
 c.Auth0OAuthenticator.oauth_callback_url = os.environ['AUTH0_CALLBACK_URL']
 c.Auth0OAuthenticator.auth0_subdomain = os.environ['AUTH0_DOMAIN']
-c.JupyterHub.authenticator_class = 'oauthenticator.auth0.Auth0OAuthenticator'
 
-# Development authenticator
+# Development authenticator (comment out above and uncomment below to enable)
 # c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 
+# Data configuration
 data_dir = os.environ['DATA_VOLUME_CONTAINER']
 c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secret')
 c.JupyterHub.db_url = os.path.join(data_dir, 'jupyterhub.sqlite')
 
-# Whitlelist users and admins
-c.Authenticator.whitelist = whitelist = set()
-c.Authenticator.admin_users = admin = set()
+# User Configuration
+allowed_users = set()
+admin = set()
 c.JupyterHub.admin_access = True
-# comma-separated admin emails, lowercased
-admin_emails = [email.strip().lower() for email in os.environ.get('ADMIN_EMAILS', '').split(',')]
-ff_users = ff_utils.search_metadata('search/?type=User&field=email', key=ff_keys)
+# Grab THE FIRST of potentially many comma-separated admin emails, lower-cased
+# Change this when >1 admin is desired - Will 21 Jan 2022
+admin_emails = [email.strip().lower() for email in os.environ.get('ADMIN_EMAILS', '').split(',')[0]]
+
+# Grab email, lab info on all users
+ff_users = ff_utils.search_metadata('search/?type=User&field=email&field=lab', key=ff_keys)
 for ff_user in ff_users:
-    if not ff_user.get('email'):
+
+    # Allow users into JH if they have both email and lab fields
+    if not ff_user.get('email') or not ff_user.get('lab'):
         continue
-    whitelist.add(ff_user['email'])
-    # base admin off of a set environment variable, for now
+    allowed_users.add(ff_user['email'])
+
+    # If first admin email from before is located, add it to admin set
     if ff_user['email'].lower() in admin_emails:
         admin.add(ff_user['email'])
 
-# add API token to the instance. Use the **only** the first admin email
+# Set these sets in the hub
+c.Authenticator.allowed_users = allowed_users
+c.Authenticator.admin_users = admin
+
+# add API token to the instance. Use **only** the first admin email
+# XXX: this interaction is deprecated and needs to be redone using c.JupyterHub.service_tokens - Will 21 Jan 2022
 if admin_emails:
     c.JupyterHub.api_tokens = {
         jh_token['secret']: admin_emails[0],
     }
 
+# Spawner options - this is the component that brings up user notebooks
+# Spawn single-user servers as Docker containers
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# Ensure initialize_user_content and finalize_user_content run pre/post user container spawn
+c.Spawner.pre_spawn_hook = initialize_user_content
+c.Spawner.post_stop_hook = finalize_user_content
+
+# Propagate ONLY these variables to the user notebook processes
+c.Spawner.env_keep.extend(['FF_ACCESS_KEY', 'FF_ACCESS_SECRET',
+                           'INIT_ERR_OUTPUT', 'FF_TRACKING_ID'])
+# Limit the memory use for single-user servers
+c.Spawner.mem_limit = '1G'
+
+# Spawn containers from the 4dn-docker-v2 image
+c.DockerSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
+# default `start_singleuser.sh` is included and is what we use - can be overridden
+# in docker-compose.yml
+c.DockerSpawner.cmd = os.environ.get('DOCKER_SPAWN_CMD', 'start-singleuser.sh')
+
+# Connect containers to this Docker network
+# network_name = 'bridge'  # unused?
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+
+# disable new escape behavior, see https://github.com/jupyterhub/dockerspawner/pull/414
+c.DockerSpawner.escape = 'legacy'
+
+# Point to notebook storage
+notebook_dir = os.environ['DOCKER_NOTEBOOK_DIR']
+c.DockerSpawner.notebook_dir = notebook_dir
+
+# Mount volumes from host machine
+# https://stackoverflow.com/questions/51330356/jupyterhub-in-docker-container-not-able-to-connect-to-external-directory
+# notebook_mount_dir in form: '/path/on/host'
+notebook_mount_dir = '/home/ubuntu/data/jupyterhub-fourfront-notebooks/user-{username}'
+raw_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_RAW_FILE_BUCKET']
+open_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_OPEN_DATA_BUCKET']
+proc_data_mount_dir = '/home/ubuntu/data/' + os.environ['AWS_PROC_FILE_BUCKET']
+# notebook_dir in form: '/path/on/container'
+c.DockerSpawner.volumes = {notebook_mount_dir: {'bind': notebook_dir, 'mode': 'rw'},
+                           raw_data_mount_dir: {"bind": '/home/jovyan/raw_data', 'mode': 'ro'},
+                           open_data_mount_dir: {"bind": '/home/jovyan/open_data', 'mode': 'ro'},
+                           proc_data_mount_dir: {'bind': '/home/jovyan/proc_data', 'mode': 'ro'}}
+
+# allow escaped characters in volume names
+c.DockerSpawner.format_volume_name = dockerspawner.volumenamingstrategy.escaped_format_volume_name
+# Remove containers once they are stopped
+c.DockerSpawner.remove = True
+# For debugging arguments passed to spawned containers
+c.DockerSpawner.debug = True
